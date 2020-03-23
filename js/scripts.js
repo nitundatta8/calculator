c33a738951e1e85bac2e4ebf8c3740b8f1c37a36
nitunwa <nitun.2@gmail.com>
2020-03-23 14:11:40 -0700

    test commit

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1-number2;
+};
+
+var divide = function(number1, number2) {
+  return number1/number2;
+}
+var multy = function(number1, number2) {
+  return number1* number2;
+}
+
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = multy(number1, number2);
+alert(result);
+
+
+
