1fd4dd6d02348e268624a6e2f89706870b39d8c1
nitunwa <nitun.2@gmail.com>
2020-03-25 13:02:18 -0700

    add changes to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,17 @@
+// Business (or back-end) logic:
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
 
 var subtract = function(number1, number2) {
-  return number1-number2;
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
 };
 
 var divide = function(number1, number2) {
-  return number1/number2;
-}
-var multy = function(number1, number2) {
-  return number1* number2;
-}
-
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = multy(number1, number2);
-alert(result);
-
-var degreesF = prompt("What is the temp in degrees F?");
-
-function tempConvert(degreesF) {
-  return alert("The temperature is " +(degreesF-32)*(5/9)) + "degrees Celsius.");
-}
-
-tempConvert(degreesF);
-
-
+  return number1 / number2;
+};
